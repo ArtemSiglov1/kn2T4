b0a844985830cc4c7fd1975e916b71b81a334051
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2023-11-05 15:16:39 +0200

    Add files via upload

---
+++ з1 в8/з1 в8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+
+ StringBuilder sb = new StringBuilder(Console.ReadLine());
+
+char[] arr = new char[sb.Length];
+
+sb.CopyTo(0, arr, 0, sb.Length);
+
+for (int i = arr.Length - 1; i >= 0; i--)
+
+{
+
+    Console.WriteLine("arrc[{0}] = {1}", i, arr[i]);
+
+}
+
