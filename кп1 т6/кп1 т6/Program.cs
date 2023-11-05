3306da3935116bf46fd49f812faa116ae08369ed
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2023-11-05 14:44:04 +0200

    Add files via upload

---
+++ кп1 т6/кп1 т6/Program.cs
@@ -0,0 +1,151 @@
+﻿//T6kn1
+
+//1
+
+//int i = 1;
+
+//Console.Write("Введите число конца интервала:");
+
+//int n=int.Parse(Console.ReadLine());
+
+//for(int i=1; i<=n; i++)
+
+//{
+
+//    Console.WriteLine(i);
+
+//}
+
+
+
+//while (n >= i)
+
+//{
+
+//    Console.WriteLine(i);
+
+//    i++;
+
+//}
+
+
+
+//do
+
+//{
+
+//    Console.WriteLine(i);
+
+//    i++;
+
+//}
+
+//while (n>=i);
+
+
+
+//2
+
+//int s = 1;
+
+//for (int i = 10;i <=99;i+=2) 
+
+//{
+
+//Console.WriteLine(i);
+
+//    s++;
+
+//}
+
+//Console.WriteLine(s);
+
+
+
+
+
+//3
+
+//double h = -5;
+
+//for (h = -5; h <= 5; h += 0.2)
+
+//{
+
+//    Console.WriteLine(2 * Math.Pow(h, 2) + 4 * h - 5);
+
+//}
+
+//while (h <= 5)
+
+//{
+
+
+
+//    Console.WriteLine(2 * Math.Pow(h, 2) + 4 * h - 5);
+
+//    h += 0.2;
+
+//}
+
+//do
+
+//{
+
+//    Console.WriteLine(2 * Math.Pow(h, 2) + 4 * h - 5);
+
+//    h += 0.2;
+
+//}
+
+//while (h <= 5);
+
+
+//4
+//int sum = 0;
+
+//for (int i=0;i<=1000;i++)
+
+//{
+
+//    if(i%5==0)
+
+//    {
+
+
+
+//        sum += i;
+
+
+
+//    }
+
+
+
+//}
+
+//Console.WriteLine(sum);
+
+
+
+//5
+
+//Console.WriteLine("Введите число:");
+
+//int i=int.Parse(Console.ReadLine());
+
+//int s = 0;
+
+//while (i>0)
+
+//{
+
+//    i /= 10;
+
+//    s++;
+
+//}
+
+//Console.WriteLine(s);
+
+
