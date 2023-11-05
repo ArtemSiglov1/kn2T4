9a2195c48a238d952af25cdcddfdf2fa655a35dd
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2023-11-05 15:21:58 +0200

    Add files via upload

---
+++ кп 1 т8/кп 1 т8/Program.cs
@@ -0,0 +1,53 @@
+﻿{
+    //Задача 1
+    //Console.WriteLine("Введите x=");
+    //int x = Convert.ToInt32(Console.ReadLine());
+    //Console.WriteLine("Введите y=");
+    //int y = Convert.ToInt32(Console.ReadLine());
+    //Console.WriteLine($"Число {Max(x,y)} большее");
+
+    //Задача 2
+    //Console.WriteLine("Введите x=");
+    //double x = Convert.ToDouble(Console.ReadLine());
+    //Console.WriteLine("Введите y=");
+    //double y = Convert.ToDouble(Console.ReadLine());
+    //Console.WriteLine("Введите z=");
+    //double z = Convert.ToDouble(Console.ReadLine());
+    //Console.WriteLine($"Среднее арифметическое={Ave(x,y,z)}");
+
+    //Задача 3
+    //double s = -9;
+    //double e = 9; 
+    //double st = 3;
+    //Func(start,end,step);
+    Console.ReadKey();
+}
+
+//Задача 1
+//public static int Max(int x,int y)
+//{
+//    if(x>y)
+//    {
+//        return x;
+//    }
+//    else
+//    {
+//        return y;
+//    }
+//}
+
+//Задача 2
+//public static double Aver(double x,double y,double z)
+//{
+//    return (x+y+z) / 3;
+//}
+
+//Задача 3
+//public static void Func(double s,double e,double st)
+//{
+//    for (double x = s; x <= e; x += st
+//    {
+//        double y = 4*Math.Pow(x,2) + 5 * x - 10;
+//        Console.WriteLine($" x={x} \t y={y} ");
+//    }
+//}
