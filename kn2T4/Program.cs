bacb4493eb55595033ec05d4f0fcc7e1d7d1a1fe
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2023-11-05 14:20:44 +0200

    Добавьте файлы проекта.

---
+++ kn2T4/Program.cs
@@ -0,0 +1,69 @@
+﻿//Console.Write("Введите вершину А:");
+
+//double v1 = double.Parse(Console.ReadLine());
+
+//Console.Write("Введите вершину В:");
+
+//double v2 = double.Parse(Console.ReadLine());
+
+//Console.Write("Введите вершину С:");
+
+//double v3 = double.Parse(Console.ReadLine());
+
+//Console.WriteLine($"Периметр треугольника={v1+v2+v3} ");
+
+//double p = v1 + v2 + v3;
+
+//double p2 = p / 2;
+
+//Console.WriteLine($"Площадь треугольника={Math.Sqrt(p2*(p2 - v1)*(p2 - v2)*(p2 - v3)):f1} ");
+
+
+
+//Console.Write("Введите расстояние:");
+
+//double S = double.Parse(Console.ReadLine());
+
+//Console.Write("Введите расход бензина на 100км:");
+
+//double b = double.Parse(Console.ReadLine());
+
+//Console.Write("Введите цену бензина за литр:");
+
+//double c = double.Parse(Console.ReadLine());
+
+//double c1=b*c/100;
+
+//Console.WriteLine($"Цена поездки составила:{S*c1:f0}");
+
+
+
+
+
+
+
+//Console.Write("Введите цену 1 кг конфет:");
+
+//double k = double.Parse(Console.ReadLine());
+
+//Console.Write("Введите цену 1 кг печенья:");
+
+//double p = double.Parse(Console.ReadLine());
+
+//Console.Write("Введите цену 1 кг яблок:");
+
+//double ap = double.Parse(Console.ReadLine());
+
+//Console.Write("Купили кг конфет:");
+
+//double k1 = double.Parse(Console.ReadLine());
+
+//Console.Write("Купили кг печенья:");
+
+//double p1 = double.Parse(Console.ReadLine());
+
+//Console.Write("Купили кг яблок:");
+
+//double ap1 = double.Parse(Console.ReadLine());
+
+//Console.WriteLine($"Цена всей покупки составила:{k1*k+p1*p+ap1*ap:f2}");
