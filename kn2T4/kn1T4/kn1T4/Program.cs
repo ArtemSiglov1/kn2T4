d2a80ff53b23e72fc626ad124d91b76e913cb540
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2023-11-05 14:32:10 +0200

    Add files via upload

---
+++ kn2T4/kn1T4/kn1T4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задание
+//Console.Title = "Тема №4 Компьютерный практикум №1(Сиглов) ";
+//Console.BackgroundColor = ConsoleColor.Green;
+//Console.ForegroundColor = ConsoleColor.Blue;
+//Console.SetWindowSize(60, 25);
+//Console.WriteLine(Console.CapsLock);
+//Console.WriteLine(Console.NumberLock);
+//Console.WriteLine("Введите имя:");
+//string name=Convert.ToString(Console.ReadLine());
+//Console.WriteLine($"Здравствуйте {name}");
+Console.ReadKey();
