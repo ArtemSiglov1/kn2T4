5b79f4c8d1fd61df319be94188f7928df5c2c906
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2023-11-05 15:01:08 +0200

    Add files via upload

---
+++ кп2 т7/кп2 т7/Program.cs
@@ -0,0 +1,63 @@
+﻿//Задача 1
+//Console.Write("Введите название футбольного клуба:");
+//string c = Console.ReadLine();
+//int s= c.Length;
+//Console.WriteLine($"Количество символов в названии клуба:{s}");
+
+//Задача 2
+//Console.Write("Введите название города:");
+//string t= Console.ReadLine();
+//int count = t.Length;
+//if(t.Length%2==0)
+//{
+//    Console.WriteLine("Количество символов в городе четно");
+//}
+//else
+//{
+//    Console.WriteLine("Количество символов в городе нечетно");
+//}
+
+//Задача 3
+//Console.Write("Введите первую фамилию:");
+//string s = Console.ReadLine();
+//Console.WriteLine("Введите вторую фамилию:");
+//string s2 = Console.ReadLine();
+//int count1 = s.Length;
+//int count2 = s2.Length;
+//if(count1>count2)
+//{
+//    Console.WriteLine("Первая фамилия длиннее второй");
+//}
+//else if(count1<count2)
+//{
+//    Console.WriteLine("Вторая фамилия длиннее первой");
+//}
+//else
+//{
+//    Console.WriteLine("Фамилии одинаковы по длине");
+//}
+
+//Задача 4
+//string l = "a";
+//string[] w = new string[5];
+//for (int i = 0; i < w.Length; i++)
+//{
+//    Console.WriteLine("Ввести слова:");
+//    w[i] = Console.ReadLine();
+//    if(w0].Length<w[i].Length)
+//    {
+//        l= w[i];
+//    }
+//}
+//Console.WriteLine("Самое длинное слово:"+l);
+
+//Задача 5
+//Console.Write("Введите имя:");
+//string n = Console.ReadLine();
+//Console.Write("Введите фамилию:");
+//string s = Console.ReadLine();
+//string f=n+" "+s;
+//Console.WriteLine(f);
+
+
+Console.ReadKey();
