9a1a132a9310c2ab80fcbc405e9b89fa4ffe5b24
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2023-11-05 15:26:42 +0200

    Add files via upload

---
+++ т8 кп2/т8 кп2/Program.cs
@@ -0,0 +1,60 @@
+﻿{ 
+//Задача 1
+//symbol();
+
+//Задача 2
+//Treyg();
+
+//Задача 3
+//double y = Func(5)+Func(12)+Func(19);
+//Console.WriteLine($"x={y}");
+
+//Задача 4
+//Console.Write("Введите a=");
+//int a=Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите b=");
+//int b=Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"Число {Max(a,b)} большее");
+Console.ReadKey();
+        }
+
+        //Задача 1
+        //public static void symbol()
+        //{
+        //    for(int i=0;i<60;i++)
+        //    {
+        //        Console.Write("*");
+        //    }
+        //}
+
+        //Задача 2
+        //public static void Treyg()
+        //{
+        //    for (int i = 0; i < 8; i++)
+        //    {
+        //        for (int j = 0; j <i; j++)
+        //        {
+        //            Console.Write("#");
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+
+        //Задача 3
+        //public static double Func(double x)
+        //{
+        //    return (Math.Sqrt(x)+x)/2;
+        //}
+
+        //Задача 4
+        //public static int Max(int a, int b)
+        //{
+        //    if(a>b)
+        //    {
+        //        return a;
+        //    }
+        //    else
+        //    { 
+        //        return b;
+        //    }
+        //}
