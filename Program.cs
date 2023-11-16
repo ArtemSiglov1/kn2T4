4503ebedc3a3b35f5dc4e5310488fd7b1be08a54
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2023-11-16 19:02:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿{
+    //1 4
+
+    //Str();
+
+    //2 4
+
+    //3 4
+
+    //double x = Func(8, 5) + Func(6, 12) + Func(7,21);
+
+    //Console.WriteLine($"x={x:f1}");
+
+    //4 4
+
+    //Console.Write("x=");
+
+    //int x = int.Parse(Console.ReadLine());
+
+    //Console.Write("y=");
+
+    //int y = int.Parse(Console.ReadLine());
+
+    //int z = Max(x, 2 * y) + Max(2 * x - y, y);
+
+    //Console.WriteLine($"z={z}");
+
+    //Console.ReadKey();  
+    //2 4
+    //Treyg();
+
+}
+
+//1 4
+
+//static void Str()
+
+//{
+
+//    for (int i = 0; i < 5; i++)
+
+//    {
+
+//        Console.WriteLine("");
+
+//        for (int j = 0; j < 15; j++)
+
+//        {
+
+//            Console.Write("*");
+
+//        }
+
+//    }
+
+//}
+
+//3 4
+
+//static double Func(int x,int y)
+
+//{
+
+//    return (Math.Sqrt(x)+y)/(Math.Sqrt(y)+x);
+
+//}
+
+//4 4
+
+//       static int Max(int x, int y)
+
+//{
+
+//    if (x > y)
+
+//    {
+
+//        return x;
+
+//    }
+
+//    else
+
+//    {
+
+//        return y;
+
+//    }
+
+//}
+//2 4
+//static void Treyg()
+//{
+//    int k = 14;
+//    int u = 2;
+//    for (int s = 0; s < 25; s++)
+//    {
+//        if (s >= 4)
+//        {
+//            if (u <= 15)
+//            {
+//                u++;
+//                for (int j = 2; j <= u; j++)
+//                {
+//                    Console.Write(" ");
+//                }
+//            }
+
+//            for (int i = 0; i < k; i++)
+//            {
+//                Console.Write("#");
+//            }
+//            k--;
+//        }
+//        Console.WriteLine("");
+
+//    }
+//}
