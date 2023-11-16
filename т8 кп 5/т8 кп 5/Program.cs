c872b5cc04fb996911fe48e952208083e1612527
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2023-11-16 20:36:55 +0200

    Add files via upload

---
+++ т8 кп 5/т8 кп 5/Program.cs
@@ -0,0 +1,59 @@
+﻿//1
+//static double Func(int x)
+//{
+    // double y = (x + Math.Sin(x))/3;
+    // return y; 
+    //}
+    //2
+    //static int Seconds(int h,int m, int s)
+    //{
+    //m += h * 60; 
+    //s += m * 60; 
+    // return h*3600+m*60+s; 
+    //} 
+    //5 2 
+    //static void Numbers(int n) 
+    //{ 
+    // for (int i = 2; i <= n; i++) 
+    // { 
+    // if (i % 2 == 0) 
+    // { 
+    // Console.WriteLine(i); 
+    // } 
+    // } 
+    //} 
+    //5 3
+    //static double Avarage(int a, int b)
+    //{
+    //int s = 0;
+    //for (int i = a; i <= b; i++)
+    //{
+    //s += i;
+    //}
+    //return s/b;
+    //}
+    //static void Main(string[] args)
+    //{ 
+    //1 
+    //double y = (1 + Math.Sin(1) + 5 + Math.Sin(5) + 7 + Math.Sin(7)) / 3;
+    //double z = Func(1)+Func(5)+Func(7);
+    //Console.WriteLine(z);
+    //Console.WriteLine(y);
+    //2 
+    //Console.Write("Введите h:"); 
+    //int h = int.Parse(Console.ReadLine()); 
+    //Console.Write("Введите m:");
+    //int m = int.Parse(Console.ReadLine());
+    //Console.Write("Введите s:");
+    //int s = int.Parse(Console.ReadLine());
+    //int z = Seconds(h, m, s); 
+    //Console.WriteLine($"z={z}");
+    //5 2 
+    //Console.Write("Введите n: ");
+    //int n = int.Parse(Console.ReadLine());
+    //Numbers(n); //5 3 Console.Write("Введите a:");
+    //int a = int.Parse(Console.ReadLine());
+    //Console.Write("Введите b:");
+    //int b = int.Parse(Console.ReadLine());
+    //double z=Avarage(a, b);
+    //Console.WriteLine(z); Console.ReadKey();
