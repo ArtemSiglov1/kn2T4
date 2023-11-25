5c349c382c3ec13e7f1ca23d4c678d74267ba847
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2023-11-25 16:33:15 +0200

    Add files via upload

---
+++ T9кп2/T9кп2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T9кп2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //static void Main(string[] args)
+
+            //{
+
+            //    List<string> list = new List<string>();
+
+            //    list.Add("Катя");
+
+            //    list.Add("Надя");
+
+            //    list.Add("Лера");
+
+            //    list.Add("Оля");
+
+            //    list.Add("Лера");
+
+            //    list.Add("Дина");
+
+            //    Show(list);
+
+            //    Show1(list);
+
+            //    Console.WriteLine($"текущее количество элементов коллекции-{list.Count}");
+
+            //    list.Insert(list.Count, "Арина");
+
+            //    Show(list);
+
+            //    list.Insert(0, "Юля");
+
+            //    Show(list);
+
+            //    list.RemoveAt(2);
+
+            //    Show(list);
+
+            //    Back(list);
+
+
+
+            //    Console.ReadLine();
+
+            //}
+
+            //static void Show(List<string> list)
+
+            //{
+
+            //    Console.WriteLine($"\n");
+
+            //    foreach (string item in list)
+
+            //    {
+
+            //        Console.WriteLine(item);
+
+            //    }
+
+            //}
+
+            //static void Show1(List<string> list)
+
+            //{
+
+            //    Console.WriteLine($"\n");
+
+            //    foreach (string item in list) 
+
+            //    {
+
+            //     Console.WriteLine(item.ToUpper());
+
+            //    }
+
+            //}
+
+            //static void Back(List<string> list)
+
+            //{
+
+            //    Console.WriteLine($"\n");
+
+            //    for (int i = list.Count-1;i >= 0;i--)
+
+            //    {
+
+            //        Console.WriteLine(list[i]);
+
+            //    }
+
+            //}
+        }
+    }
+}
