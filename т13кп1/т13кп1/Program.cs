29cca38b75f9e96b1d4c5c28adf3f8a307e5dc26
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2023-12-16 18:18:39 +0200

    Add files via upload

---
+++ т13кп1/т13кп1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace т13кп1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Иванов", 3);
+            Student student2 = new Student("", -7);
+            string f = Console.ReadLine();
+            int k = int.Parse(Console.ReadLine());
+            Student student1 = new Student(f, k);
+
+            Console.WriteLine($"{student.ToString()}\n{student2.ToString()}\n{student1.ToString()}");
+            Avto avto = new Avto("bmw", "red", 10);
+            Avto avto1 = new Avto("audi", "green", 45);
+            Console.WriteLine($"{avto.ToString()}\n{avto1.ToString()}");
+            Computer computer = new Computer();
+            computer.Model = Console.ReadLine();
+            computer.Ram = int.Parse(Console.ReadLine());
+            computer.Hdd = int.Parse(Console.ReadLine());
+            Computer computer1 = new Computer();
+            computer1.Model = Console.ReadLine();
+            computer1.Ram = int.Parse(Console.ReadLine());
+            computer1.Hdd = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{computer.ToString()}\n{computer1.ToString()}");
+            Tovar tovar = new Tovar();
+            tovar.Name = Console.ReadLine();
+            tovar.Price = int.Parse(Console.ReadLine());
+            tovar.Kvo = int.Parse(Console.ReadLine());
+
+            Tovar tovar1 = new Tovar();
+            tovar1.Name = Console.ReadLine();
+            tovar1.Price = int.Parse(Console.ReadLine());
+            tovar1.Kvo = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{tovar.ToString()}\n{tovar1.ToString()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ т13кп1/т13кп1/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace т13кп1
+{
+    internal class Student
+    {
+        
+            private string fam;
+            private int kurs;
+
+            public string Fam
+            {
+
+
+                get
+                {
+                    return (fam != "") ? fam : "Неизвестно";
+                }
+                set { fam = value.ToUpper(); }
+
+
+
+            }
+            public int Kurs
+            {
+                set
+                {
+                    if (value < 1 || value > 4)
+
+                        Console.WriteLine("Такого курса не бывает");
+
+                    else
+
+                        kurs = value;
+
+                    //kurs=(value<1||value>4)?0:value;
+
+                }
+                get { return kurs; }
+            }
+
+            public Student() { }
+            public Student(string fam, int kurs)
+            {
+                this.Fam = fam;
+                this.Kurs = kurs;
+            }
+            public override string ToString()
+            {
+                return $"Фамилия-{Fam}\tKurs-{Kurs}";
+            }
+        }
+}
