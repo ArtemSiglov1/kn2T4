0cbe5342d46e34b70833edbd4d5a8d0e34997af2
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-01-28 15:25:15 +0300

    Add files via upload

--- т16кп1/т16кп1/Age.cs
+++ т16кп1/т16кп1/Age.cs
@@ -6,15 +6,39 @@ using System.Threading.Tasks;
 
 namespace т16кп1
 {
+    /// <summary>
+    /// класс описывающий возраст человека
+    /// </summary>
     internal class Vozr
     {
+        /// <summary>
+        /// поля описывающие дату рождения человека
+        /// </summary>
         int year,month,day;
-        
+        /// <summary>
+        /// свойство для поля год рождения
+        /// </summary>
         public int Year { get => year; set => year = value; }
+        /// <summary>
+        /// свойство для поля месяц рождения
+        /// </summary>
         public int Month { get => month; set => month = value; }
+        /// <summary>
+        /// свойство для поля день рождения 
+        /// </summary>
         public int Day { get => day; set => day = value; }
+        /// <summary>
+        /// конструктор по умолчанию
+        /// </summary>
 
         public Vozr() { }
+        /// <summary>
+        /// конструктор с параметрами
+        /// </summary>
+        /// <param name="year">год рождения</param>
+        /// <param name="month">месяц рождения</param>
+        /// <param name="day">день рождения </param>
+        /// <param name="date">сегодняшняя дата</param>
         public Vozr(int year, int month, int day,DateTime date) 
         {
          this.Year=year;
