374b9ade50eca9fe912863a6b671d3ccab0cff3a
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-02-01 11:18:31 +0200

    Add files via upload

---
+++ т16кп4/т16кп4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace т16кп4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 =DateTime.Now;
+            Staff[] staffs = new Staff[1];
+            for(int i=0;i<staffs.Length;i++)
+            {
+                string surname=Console.ReadLine(), name=Console.ReadLine(), otchestvo=Console.ReadLine(), dol=Console.ReadLine();
+                DateTime date=DateTime.Parse(Console.ReadLine()),date2=DateTime.Parse(Console.ReadLine());
+                double oklad=double.Parse(Console.ReadLine());
+                Console.Clear();
+                staffs[i] = new Staff(surname,name,otchestvo,date,dol,oklad,date1,date2);
+            }
+            foreach(var elem in staffs)
+            {
+                Console.Write($"{Staff.tabNom} {elem.ToString()}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ т16кп4/т16кп4/Staff.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace т16кп4
+{
+    class Staff
+    {
+        public static int tabNom;
+        /// <summary>
+        /// 
+        /// </summary>
+        string surname, name, otchestvo,dol;
+        DateTime date,date1,date3;
+        double oklad;
+        string d;
+        public string Surname { get => (surname==""?"Имени нет":surname); set => surname = value; }
+        public string Name { get => (name==""?"имени нет":name); set => name = value; }
+        public string Otchestvo { get => (otchestvo==""?"Отчества нет":otchestvo); set => otchestvo = value; }
+        public string Dol { get => (dol==""?"Должность не указана":dol); set => dol = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public double Oklad { get => oklad; set => oklad = value; }
+        public DateTime Date1 { get => date1; set => date1 = value; }
+        public DateTime Date3 { get => date3; set => date3 = value; }
+
+        public Staff() { }
+        public Staff(string surname, string name, string otchestvo,DateTime date, string dol, double oklad,DateTime date1,DateTime date3)
+        {
+            this.Surname = surname;
+            this.Name = name;
+            this.Otchestvo = otchestvo;
+            this.Date = date;
+            this.Dol = dol;
+            this.Oklad = oklad;
+            this.Date1 = date1;
+            this.Date3 = date3;
+
+            if (date1.Month < date.Month)
+            {
+                d=date1.AddYears(-date.Year - 1).ToString("yy");
+            }
+            else if (date1.Month == date.Month)
+            {
+                if (date1.Day < date.Day)
+                {
+                  d= date1.AddYears(-date.Year - 1).ToString("yy");
+                }
+                else
+                {
+                   d= date1.AddYears(-date.Year).ToString("yy");
+                }
+            }
+            else
+            {
+                d = date1.AddYears(-date.Year).ToString("yy");
+            }
+            tabNom++;
+        }
+        static Staff()
+        {
+            tabNom = 0;
+        }
+        public override string ToString()
+        {
+            return $"{surname} {name} {otchestvo} {date.ToLongDateString()} {dol} {oklad} Возраст-{d} поступил-{date3}  отработанно-{(date1.Subtract(date3)).Days} \n";
+        }
+
+    }
+}
