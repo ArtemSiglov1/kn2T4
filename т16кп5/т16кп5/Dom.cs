1a88a36d9efef06283c2241cb88764410716a4a3
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-02-03 21:14:41 +0200

    Add files via upload

--- т16кп5/т16кп5/Dom.cs
+++ т16кп5/т16кп5/Dom.cs
@@ -6,14 +6,38 @@ using System.Threading.Tasks;
 
 namespace т16кп5
 {
+    /// <summary>
+    /// класс описывающий кондиционер в умном доме 
+    /// </summary>
     internal class Dom
     {
+           /// <summary>
+           /// дата включения
+           /// </summary>
         DateTime DateVkl {  get; set; }
+        /// <summary>
+        /// время включения
+        /// </summary>
         DateTime TimeVkl { get; set; }
+        /// <summary>
+        /// температурный режим
+        /// </summary>
         string TempRez { get; set; }
+        /// <summary>
+        /// температура воздуха
+        /// </summary>
         int TempVozd {  get; set; }
-
+        /// <summary>
+        /// конструктор по умолчанию 
+        /// </summary>
         public Dom() { }
+        /// <summary>
+        /// конструктор с параметрами
+        /// </summary>
+        /// <param name="dateVkl">дата включения</param>
+        /// <param name="timeVkl">время включения</param>
+        /// <param name="tempRez">темп режим</param>
+        /// <param name="tempVozd">темп воздуха</param>
         public Dom(DateTime dateVkl, DateTime timeVkl, string tempRez, int tempVozd)
         {
             DateVkl = dateVkl;
@@ -21,10 +45,17 @@ namespace т16кп5
             TempRez = tempRez;
             TempVozd = tempVozd;
         }
+        /// <summary>
+        /// метод для вывода информации о включении кондиционера
+        /// </summary>
+        /// <returns>Дата включения, время включения, темп режим, темп воздуха, остаток времени до включения кондеционера</returns>
         public override string ToString()
         {
             return $"Дата включения-{DateVkl.ToLongDateString()} Время включения-{TimeVkl.ToLongTimeString()} Темп режим-{TempRez} Темп воздуха-{TempVozd} Осталось до включения-{(TimeVkl-DateTime.Now ).TotalMinutes}";
         }
+        /// <summary>
+        /// метод для вывода информации о включении кондиционера заплонированного на завтра
+        /// </summary>
         public void PlanRez()
         {
             if (DateVkl>DateTime.Now && DateVkl < DateTime.Now.AddDays(2) )
--- т16кп5/т16кп5/Web.cs
+++ т16кп5/т16кп5/Web.cs
@@ -6,15 +6,43 @@ using System.Threading.Tasks;
 
 namespace т16кп5
 {
+    /// <summary>
+    /// Класс описывающий посещение сайта
+    /// </summary>
     internal class Web
     {
-        
+        /// <summary>
+        /// адресс сайта
+        /// </summary>
         string Address { get; set; }
+        /// <summary>
+        /// ip устройства
+        /// </summary>
         string Ip {  get; set; } 
+        /// <summary>
+        /// браузер пользователя
+        /// </summary>
             string WebClient{get;set;}
+        /// <summary>
+        /// дата входа
+        /// </summary>
         DateTime DateVisit { get; set; }
+        /// <summary>
+        /// время входа
+        /// </summary>
             DateTime TimeVisit {  get; set; }
+        /// <summary>
+        /// конструктор по умолчанию
+        /// </summary>
         public Web() { }
+        /// <summary>
+        /// конструктор с параметрами
+        /// </summary>
+        /// <param name="address">адрес сайта</param>
+        /// <param name="ip">ip пользов</param>
+        /// <param name="webClient">браузер пользов</param>
+        /// <param name="dateVisit">дата визита</param>
+        /// <param name="timeVisit">время визита</param>
         public Web(string address, string ip, string webClient, DateTime dateVisit, DateTime timeVisit)
         {
             this.Address = address;
@@ -23,10 +51,19 @@ namespace т16кп5
             this.DateVisit = dateVisit;
             this.TimeVisit = timeVisit;
         }
+        /// <summary>
+        /// метод для определения дня недели
+        /// </summary>
+        /// <returns>день недели от даты посещения сайта</returns>
         public DayOfWeek Day()
         {
             return DateVisit.DayOfWeek;
         }
+        /// <summary>
+        /// метод для вывода информации о посещении сайта в заданый временной промежуток 
+        /// </summary>
+        /// <param name="time1"></param>
+        /// <param name="time2"></param>
         public void Vkl(DateTime time1,DateTime time2) 
         {
          if(time1>DateVisit&&DateVisit<time2)
