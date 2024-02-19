2e7d7338e73b2ce5b5de15e067af831171573dc2
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-02-19 09:50:08 +0200

    Add files via upload

---
+++ 19.02.2024(oop)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19._02._2024_oop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
