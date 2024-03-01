852e629f9a6fb17fbae076515f3426aeda39a968
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-03-01 15:16:00 +0300

    Add files via upload

---
+++ 01.03.2024/Abiturient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._03._2024
+{
+    internal class Abiturient:Person
+    {
+        public string Facult {  get; set; }
+        public Abiturient() { }
+        public Abiturient(string name,string surname,DateTime dateB,string facult):base(name, surname, dateB)
+        {
+            Facult = facult;
+        }
+
+        public override int Age()
+        {
+           DateTime dateNow=DateTime.Now;
+            if (dateNow.Month<=DateB.Month)
+            {
+                return dateNow.Year-DateB.Year-1;
+            }
+            else if(dateNow.Month==DateB.Month)
+            {
+                if (dateNow.Day<=DateB.Day)
+                {
+                    return dateNow.Year - DateB.Year - 1;
+                }
+                else
+                {
+                    return dateNow.Year - DateB.Year;
+                }
+                
+            }
+            else { return dateNow.Year - DateB.Year; }
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}";
+        }
+    }
+}
---
+++ 01.03.2024/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._03._2024
+{
+    abstract class Person
+    {
+        public string Name {  get; set; }
+        public string Surname { get; set; }
+        public DateTime DateB {  get; set; }
+        public Person() { }
+        public Person(string name, string surname, DateTime dateB)
+        {
+            Name = name;
+            Surname = surname;
+            DateB = dateB;
+        }
+
+        public override string ToString()
+        {
+            return $"Name-{Name}\nSurname-{Surname}\nДата рождения-{DateB}";
+        }
+        public abstract int Age();
+    }
+}
