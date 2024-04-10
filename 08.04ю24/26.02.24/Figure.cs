9c0c59b2efef6f49d55880180662cbb150fede32
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-04-10 21:49:30 +0300

    Add files via upload

---
+++ 08.04ю24/26.02.24/Figure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26._02._24
+{
+    /// <summary>
+    /// абстрактный класс описывающий фигуру
+    /// </summary>
+    interface IFigure
+    {   
+        /// <summary>
+        /// свойство названия фигуры
+        /// </summary>
+        string Name { get; set ; }
+        /// <summary>
+        /// абстрактный метод нахождения площади
+        /// </summary>
+        /// <returns>площадь фигуры</returns>
+         double Area();
+        /// <summary>
+        /// абстрактный метод нахождения периметра
+        /// </summary>
+        /// <returns>периметр</returns>
+         double Perimeter();
+        /// <summary>
+        /// метод для вывода информации об объекте
+        /// </summary>
+        /// <returns>поля класса</returns>
+        string ToString();
+    }
+}
